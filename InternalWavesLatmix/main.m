e23a80637b32bec5619207427a8cc08629ca7668
JeffreyEarly <jeffrey@earlyinnovations.com>
2015-03-28 09:24:12 -0700

    Initial Commit

---
+++ InternalWavesLatmix/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  InternalWavesLatmix
+//
+//  Created by Jeffrey Early on 3/28/15.
+//  Copyright (c) 2015 Early Innovations, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
