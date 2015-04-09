f88c2cf3beb4c0bf001ab4eb1e9b48a773aa86e4
Jeffrey J. Early <jeffrey@jeffreyearly.com>
2015-04-09 10:53:24 -0700

    Increasing number of vertical modes.

--- InternalWavesLatmix/main.m
+++ InternalWavesLatmix/main.m
@@ -13,11 +13,12 @@
 int main(int argc, const char * argv[])
 {
     @autoreleasepool {
+        // 60km x 15km box, set energyLevel to 1/64
         GLFloat latitude = 31;
-        GLFloat width = 60e3;
+        GLFloat width = 15e3;
         GLFloat height = 15e3;
         NSUInteger Nx = 256;
-        NSUInteger Ny = 64;
+        NSUInteger Ny = 256;
         NSUInteger Nz_in = 512; // Number of grid points upon which to project the input profile (512 rec.)
         
         NSUInteger Nz_out = 80; // Number of grid points and range for the output
@@ -25,12 +26,12 @@ int main(int argc, const char * argv[])
         GLFloat maxDepth = 0;
 		
 		BOOL shouldIncludeDiffusiveFloats = NO;
-        GLFloat maxWavePeriods = 10.0;
+        GLFloat maxWavePeriods = 7.0;
         GLFloat horizontalFloatSpacingInMeters = 125;
         GLFloat sampleTimeInMinutes = 15;
-        GLFloat energyLevel = 1./64.;
+        GLFloat energyLevel = 1./8.;
 		
-		BOOL shouldApplyStrainField = YES;
+		BOOL shouldApplyStrainField = NO;
 		GLFloat sigma = 3.52e-6;
 		GLFloat theta = -32.7;
 		GLFloat sigma_n = sigma*cos(2*theta*M_PI/180.);
